a654bab5138eec97ac592cdce379e5ac150d8ea5
Bhaven Chaudhary <chaudharybhaven@gmail.com>
2025-12-10 22:33:36 +0530

    OtpIput problem

---
+++ MachineCodingReact/src/SixDigitOtp/OtpInput.jsx
@@ -0,0 +1,53 @@
+import { useEffect, useRef, useState } from "react";
+import styles from "./OtpInput.module.css";
+
+export default function ({}) {
+  const otpLength = 6;
+  const [otp, setOtp] = useState(Array(otpLength).fill(""));
+
+  const inputRef = useRef([]);
+
+  function handelInputChange(index, event) {
+    let value = event.target.value.replace(/\D/g, "");
+
+    setOtp((prev) => {
+      let tempValue = [...otp];
+      tempValue[index] = value;
+      return tempValue;
+    });
+
+    //Move to next box automatically
+    if (index < otpLength - 1 && value) {
+      inputRef.current[index + 1].focus(); //ref of next input box
+    }
+  }
+
+  useEffect(() => {
+    //to check if all 6 inputs have value
+    let isCompleted = otp.every((val) => val !== "" && val !== null);
+
+    if (isCompleted) {
+      console.log("Otp is", otp.join(""));
+    }
+  }, [otp]);
+
+  return (
+    <div className="OtpInputContainer">
+      <h1>Enter your OTP</h1>
+      <div>
+        {otp.map((value, index) => {
+          return (
+            <input
+              ref={(el) => (inputRef.current[index] = el)}
+              value={otp[index]}
+              key={index}
+              className={styles.input}
+              maxLength={1}
+              onInput={(event) => handelInputChange(index, event)}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
+}
