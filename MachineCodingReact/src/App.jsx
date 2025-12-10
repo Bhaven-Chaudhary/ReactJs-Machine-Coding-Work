62310d4196c6ec27508b4fb52fd4d88cb86f5b5a
Bhaven Chaudhary <chaudharybhaven@gmail.com>
2025-12-10 22:33:14 +0530

    blank project

---
+++ MachineCodingReact/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import "./App.css";
+import OtpInput from "./SixDigitOtp/OtpInput";
+
+function App() {
+  return (
+    <>
+      <OtpInput />
+    </>
+  );
+}
+
+export default App;
